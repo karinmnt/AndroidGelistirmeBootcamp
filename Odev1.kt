1df0a4394b414d87877faa798f59bb22e29b95d9
Karin Manto <karinmanto@gmail.com>
2025-04-25 00:29:49 +0300

    Odev1.kt dosyası eklendi

---
+++ Odev1.kt
@@ -0,0 +1,87 @@
+package com.example.myapplication.ders1
+
+fun main() {
+    val sehir = "İstanbul"
+    val ulke = "Japonya"
+    val telefon ="+90 504 718 9875"
+    val posta_kodu = 34152
+    val email = "ornekmail@mail.com"
+    val meslek = "mühendis"
+    val stok_miktari = 3000
+    val musteri_adi = "Eren"
+    val bakiye = 1500
+    val dogum_gunu = "07/09/1976"
+    val maas = 50000
+    val medeni_durum = "Bekar"
+    val urun_yorumu = "Kaliteli."
+    val odeme_tarihi = "30/04/2025"
+    val odeme = 290
+    val siparis_adeti = 50
+    val araba_modeli = "Audi R8"
+    val kitap_adi = "Dune"
+    val yayinlanma_tarihi = 1965
+    val indirim_miktari = "%10"
+    val oda_sayisi = 8
+    val enlem = "36-42"
+    val boylam = "26-45"
+    val urun_adi = "anahtarlık"
+    val yemek_fiyati = 329.99
+    val marka = "Eti"
+    val muzik_adi = "Bu Son Olsun"
+    val video_suresi = 23
+    val urun_puani = 4.8
+    val resim_adi = "urun.jpg"
+    val dosya_formati = "JPEG"
+    val renk = "mavi"
+    val renk_kodu = "007FFF"
+    val telefon_modeli = "Samsung Galaxy S25"
+    val ekran_boyutu ="5.5 inç"
+    val agirlik = "120 gram"
+    val ulusal_gun = "23 Nisan"
+    val tatil_gunu = "01 Mayıs"
+    val rezervasyon_tarihi = "06/06/2025"
+    val sokak_adi = "Çiçek sokak"
+    val otobus_hatti = "72T"
+    val kalan_dakika = "3 dk"
+    val takip_kodu = "3578TKL"
+    val kupon_suresi = "3 gün"
+    val kupon_kodu = "INDIRIM10"
+    val fatura_adresi = "Cumhuriyet Mah., No:12, Beşiktaş/İstanbul"
+
+    println("Şehir: $sehir")
+    println("Ülke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta kodu: $posta_kodu")
+    println("E-mail: $email")
+    println("Meslek: $meslek")
+    println("Stok miktarı: $stok_miktari")
+    println("Müşteri adı: $musteri_adi")
+    println("Bakiye: $bakiye TL")
+    println("Doğum günü: $dogum_gunu")
+    println("Maaş: $maas")
+    println("Medeni durum: $medeni_durum")
+    println("Ürün yorumu: $urun_yorumu")
+    println("Ödeme tarihi: $odeme_tarihi, Ödeme:$odeme TL")
+    println("Sipariş adeti: $siparis_adeti")
+    println("Araba modeli:$araba_modeli")
+    println("Kitap adı: $kitap_adi, Yayınlanma tarihi: $yayinlanma_tarihi")
+    println("İndirim miktarı: $indirim_miktari")
+    println("Oda sayısı: $oda_sayisi")
+    println("Türkiye enlemleri : $enlem, Türkiye boylamları: $boylam")
+    println("Ürün adı: $urun_adi")
+    println("Yemek fiyatı: $yemek_fiyati")
+    println("Marka: $marka")
+    println("Müzik adı: $muzik_adi")
+    println("Video süresi: $video_suresi")
+    println("Ürün puanı: $urun_puani")
+    println("Resim adı: $resim_adi, Dosya formatı: $dosya_formati")
+    println("Renk: $renk, renk kodu: $renk_kodu")
+    println("Telefon modeli : $telefon_modeli, Ekran boyutu = $ekran_boyutu, Ağırlık = $agirlik")
+    println("Ulusal gün: $ulusal_gun")
+    println("Tatil günü : $tatil_gunu, Rezervasyon tarihi : $rezervasyon_tarihi")
+    println("Sokak adı: $sokak_adi")
+    println("Otobüs hattı: $otobus_hatti, Kalan dakika: $kalan_dakika")
+    println("Takip kodu: $takip_kodu")
+    println("Kupon süresi: $kupon_suresi, Kupon kodu: $kupon_kodu")
+    println("Fatura adresi: $fatura_adresi")
+}
